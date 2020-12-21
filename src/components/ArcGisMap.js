f049df8d447a55c0c5966acb1e9160be16fd7099
Fabricio Bezerra <fabricio.spb@gmail.com>
2020-12-20 16:30:50 -0800

    add popupTemplate to FeatureLayer

--- src/components/ArcGisMap.js
+++ src/components/ArcGisMap.js
@@ -23,9 +23,37 @@ const ArcGisMap = () => {
         zoom: 12, // Sets the zoom LOD to 11
       });
 
+      const popupTemplate = {
+        // autocasts as new PopupTemplate()
+        title: "Fire details",
+        content: [
+          {
+            // It is also possible to set the fieldInfos outside of the content
+            // directly in the popupTemplate. If no fieldInfos is specifically set
+            // in the content, it defaults to whatever may be set within the popupTemplate.
+            type: "fields",
+            fieldInfos: [
+              {
+                fieldName: "ADDRESS",
+                label: "Address",
+              },
+              {
+                fieldName: "ZIP",
+                label: "Zip Code",
+              },
+              {
+                fieldName: "YEAR_",
+                label: "Year",
+              },
+            ],
+          },
+        ],
+      };
+
       const layer = new FeatureLayer({
         url:
           "https://services1.arcgis.com/F1v0ufATbBQScMtY/ArcGIS/rest/services/FireIncidents/FeatureServer/2",
+        popupTemplate,
       });
 
       map.add(layer);
