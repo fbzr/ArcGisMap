3ad7b39f82b3bb0be7327d344f974c15eb1d3e62
Fabricio Bezerra <fabricio.spb@gmail.com>
2021-01-18 22:53:57 -0300

    add feature layer to map

--- src/controllers/Map.tsx
+++ src/controllers/Map.tsx
@@ -4,13 +4,15 @@ import { setDefaultOptions, loadModules } from "esri-loader";
 class MapController {
   #map?: __esri.Map;
   #mapView?: __esri.MapView;
+  #featureLayer?: __esri.FeatureLayer;
 
   initialize = async (domRef: RefObject<HTMLDivElement>) => {
     if (!domRef.current) return;
 
-    const [Map, MapView] = await loadModules([
+    const [Map, MapView, FeatureLayer] = await loadModules([
       "esri/Map",
       "esri/views/MapView",
+      "esri/layers/FeatureLayer",
     ]);
 
     this.#map = new Map({ basemap: "topo-vector" });
@@ -21,6 +23,24 @@ class MapController {
       center: [-115.1398, 36.1699], // Sets the center point of the view at a specified lon/lat - Las Vegas: 36.1699° N, 115.1398° W
       zoom: 12,
     });
+
+    this.#featureLayer = new FeatureLayer({
+      url:
+        "https://services1.arcgis.com/F1v0ufATbBQScMtY/ArcGIS/rest/services/FireIncidents/FeatureServer/2",
+      // popupTemplate,
+      // outFields: ["*"],
+      outFields: [
+        "LOCADDRESS",
+        "ZIP",
+        "zip",
+        "alarmdate",
+        "incidentnumber",
+        "latitude",
+        "longitude",
+      ],
+    });
+
+    this.#map?.layers.add(this.#featureLayer as __esri.Layer);
   };
 }
 
